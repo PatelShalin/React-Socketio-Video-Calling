131fb5895b38bd84f2848e43fc489c196e3b32ab
PatelShalin <patel.shalin@outlook.com>
2021-01-17 17:05:22 -0500

    first

---
+++ build/precache-manifest.a4fd24a4ec2d14321bbefc55f57b8b71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1d960ffd5bbba4fb877f7337c272b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73727c97e056801a4aaa",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "15d478e74ce7375cc477",
+    "url": "/static/js/2.6d58bd24.chunk.js"
+  },
+  {
+    "revision": "f24b473656bdcdf7a40306c1c14414ef",
+    "url": "/static/js/2.6d58bd24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73727c97e056801a4aaa",
+    "url": "/static/js/main.f836ffd2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
